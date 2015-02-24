a2f2cf5c29a096f9b479c2831312a3562a4408fb
Ali Al Kuwari <alikuwar@qatar.cmu.edu>
2015-02-24 19:26:55 +0300

    all business and user options working
    was able to get edit and delete for both users and businesses to work

--- junior_project_app/app/app/controllers/businesses_controller.rb
+++ junior_project_app/app/app/controllers/businesses_controller.rb
@@ -43,25 +43,21 @@ class BusinessesController < ApplicationController
   # PATCH/PUT /businesses/1
   # PATCH/PUT /businesses/1.json
   def update
-    respond_to do |format|
-      if @business.update(business_params)
-        format.html { redirect_to @business, notice: 'Business was successfully updated.' }
-        format.json { render :show, status: :ok, location: @business }
-      else
-        format.html { render :edit }
-        format.json { render json: @business.errors, status: :unprocessable_entity }
-      end
+    @business = Business.find(params[:id])
+    if @business.update_attributes(business_params)
+      flash[:success] = "Profile updated"
+      redirect_to @business
+    else
+      render 'edit'
     end
   end
 
   # DELETE /businesses/1
   # DELETE /businesses/1.json
   def destroy
-    @business.destroy
-    respond_to do |format|
-      format.html { redirect_to businesses_url, notice: 'Business was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    Business.find(params[:id]).destroy
+    flash[:success] = "Business deleted."
+    redirect_to businesses_url
   end
 
   private
