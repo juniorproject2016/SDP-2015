158862e2170b9f9e96c9339627cb1b23b25b9115
Ali Al Kuwari <alikuwar@qatar.cmu.edu>
2015-03-25 12:55:17 +0300

    added fahim's work on home page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,9 +3,11 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( bootmetro.css )
+Rails.application.config.assets.precompile += %w( bootmetro-responsive.css )
+Rails.application.config.assets.precompile += %w( bootmetro-icons.css )
+Rails.application.config.assets.precompile += %w( bootmetro-ui-light.css )
+Rails.application.config.assets.precompile += %w( datepicker.css )
